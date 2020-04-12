66fe7a6eb7a161ceb86d6bd71c8f871bb8f77008
alex <camward@yandex.ru>
2020-04-12 15:29:21 +0400

    @PathVariable. Параметры в url

--- src/main/java/com/example/demo/controllers/ParamsController.java
+++ src/main/java/com/example/demo/controllers/ParamsController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.RequestParamDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class ParamsController {
 
     @GetMapping("/params/requestParams")
     public ResponseEntity requestParamTest(@RequestParam("id") String id, @RequestParam("name") String name) {
+        return new ResponseEntity(new RequestParamDTO(id, name), HttpStatus.OK);
+    }
+
+    @GetMapping("params/{id}/{name}")
+    public ResponseEntity pathVariableTest(@PathVariable("id") String id, @PathVariable("name") String name) {
         return new ResponseEntity(new RequestParamDTO(id, name), HttpStatus.OK);
     }
 }
